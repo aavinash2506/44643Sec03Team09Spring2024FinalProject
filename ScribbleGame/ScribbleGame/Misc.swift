d6994b5aba7928946332993864ea320ccac0f61b
vaishnavi varaganti <vaishnavivaraganti25@gmail.com>
2024-02-21 23:42:17 -0600

    Created project for Scribble Game

---
+++ ScribbleGame/ScribbleGame/Misc.swift
@@ -0,0 +1,27 @@
+//
+//  Misc.swift
+//  ScribbleGame
+//
+//  Created by Vaishnavi Varaganti on 2/21/24.
+//
+
+import UIKit
+import Foundation
+
+let everyDayObjects = 
+[
+    "acrylic paints", "alarm clock", "backpack", "battery", "bed", "bicycle", "binder", "blanket", "book",
+    "bookshelf", "broom", "bus", "calculator", "calendar", "camera", "candle", "canvas", "car", "chair",
+    "charcoal pencils", "charging cable", "clay", "clock", "coat", "coffee mug", "comb", "compass", "computer",
+    "conditioner", "desk lamp", "dish", "dishwasher", "drawing pad", "dryer", "dustpan", "e-reader", "earbuds",
+    "easel", "envelope", "eraser", "extension cord", "flashlight", "folder", "fridge", "glass", "gloves", "glue",
+    "hairbrush", "hammer", "hanger", "hat", "headphones", "ink pen", "iron", "keyboard", "keys", "lamp",
+    "laundry basket", "level", "marker", "microwave", "mop", "mouse", "nail clipper", "notebook", "oven",
+    "paintbrush", "palette", "pants", "paper", "paperclip", "pastels", "pen", "pencil", "phone", "picture frame",
+    "pillow", "plane", "plant pot", "plate", "pliers", "plunger", "post-it note", "power strip", "protractor",
+    "razor", "remote", "ruler", "saw", "scarf", "scissors", "screwdriver", "shampoo", "shirt", "shoe",
+    "silverware", "sketchbook", "sneakers", "soap", "socks", "sofa", "stapler", "stove", "sunglasses",
+    "sunglasses case", "table", "tablet", "tape", "tape measure", "thermos", "toothbrush", "toothpaste", "towel",
+    "train", "trash can", "tv", "umbrella", "vacuum cleaner", "wallet", "washing machine", "watch", "water bottle",
+    "watercolor set", "whiteboard", "wrench", "yoga mat"
+]
