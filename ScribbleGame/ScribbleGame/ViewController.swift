fa813263d65dc16e7b8a139b484a02781d22f3a4
tejeshreddy <s565694@nwmissouri.edu>
2024-02-23 16:54:08 -0600

    Added constraints to the lottie animation view

--- ScribbleGame/ScribbleGame/ViewController.swift
+++ ScribbleGame/ScribbleGame/ViewController.swift
@@ -6,9 +6,24 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scribbleLAV: LottieAnimationView!{
+        didSet{
+            scribbleLAV.loopMode = .playOnce
+            scribbleLAV.animationSpeed = 1.0
+            scribbleLAV.play{[weak self] _ in
+                          UIViewPropertyAnimator.runningPropertyAnimator(
+                              withDuration: 1.0,
+                              delay: 0.0,
+                              options: [.curveEaseInOut]){
+                              self?.scribbleLAV.alpha=0.0
+                          }
+                      }
+                  }
+              }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
