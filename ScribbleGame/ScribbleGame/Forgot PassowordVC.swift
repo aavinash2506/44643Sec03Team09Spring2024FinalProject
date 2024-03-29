a9f5d4d80d960f5aff27d7a82b631e5e305c440c
Avinash Vengalasetti <aavinash2506@gmail.com>
2024-03-28 21:59:48 -0500

    Avinash : Added background to login page

--- ScribbleGame/ScribbleGame/Forgot PassowordVC.swift
+++ ScribbleGame/ScribbleGame/Forgot PassowordVC.swift
@@ -11,7 +11,7 @@ class Forgot_PassowordVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background")!).withAlphaComponent(0.5)
         
 
         // Do any additional setup after loading the view.
